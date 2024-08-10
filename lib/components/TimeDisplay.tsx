4a4fb7cb978ab6f71d1808ea0638224972abdf37
ZeWaka <zewakagamer@gmail.com>
2024-08-10 01:13:34 -0700

    weird prop but ok

--- lib/components/TimeDisplay.tsx
+++ lib/components/TimeDisplay.tsx
@@ -1,9 +1,10 @@
-import { formatTime } from 'lib/common/format';
 import { useEffect, useRef, useState } from 'react';
 
+import { formatTime } from '../common/format';
+
 interface TimeDisplayProps {
-  /** Whether the TimeDisplay should automatically increment or decrement */
-  auto?: 'up' | 'down';
+  /** Whether the TimeDisplay should automatically increment or decrement (plain prop to dec.) */
+  auto?: 'up' | 'down' | true;
   /** An optional function to format the value */
   format?: (value: number) => string;
   /** The value that the TimeDisplay needs to render - if you pass a wrong type it will be rendered directly */
