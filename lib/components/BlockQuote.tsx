7e7159e3cc17f241d246ebe527fddf4853d19308
jlsnow301 <jlsnow301@pm.me>
2024-12-23 15:06:55 -0800

    from docs

--- lib/components/BlockQuote.tsx
+++ lib/components/BlockQuote.tsx
@@ -3,6 +3,8 @@ import { Box, type BoxProps } from './Box';
 
 /**
  * ## BlockQuote
+ * Just a block quote, just like this example in markdown:
+ * > Here's an example of a block quote.
  */
 export function BlockQuote(props: BoxProps) {
   const { className, ...rest } = props;
