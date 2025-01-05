19ee51b5d58ca90689baa3a01510e583f4aa6d15
Aylong <69762909+AyIong@users.noreply.github.com>
2025-01-05 03:58:46 +0200

    Linter

--- .storybook/previewTheme.ts
+++ .storybook/previewTheme.ts
@@ -2,11 +2,11 @@ import { create } from '@storybook/theming/create';
 import './preview.scss';
 
 export default create({
-    base: 'dark',
-    brandTitle: 'TGUI-core',
-    brandUrl: 'https://github.com/tgstation/tgui-core',
+  base: 'dark',
+  brandTitle: 'TGUI-core',
+  brandUrl: 'https://github.com/tgstation/tgui-core',
 
-    appBg: 'hsl(0, 0%, 14%)',
-    appContentBg: 'hsl(0, 0%, 14%)',
-    appPreviewBg: 'hsl(0, 0%, 14%)',
-  });
+  appBg: 'hsl(0, 0%, 14%)',
+  appContentBg: 'hsl(0, 0%, 14%)',
+  appPreviewBg: 'hsl(0, 0%, 14%)',
+});
