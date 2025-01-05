7e7e5e03367fadfdecb2519b5f51ffce16d39a3c
Aylong <69762909+AyIong@users.noreply.github.com>
2025-01-06 01:51:09 +0200

    Paint storybook to dark (#59)

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,4 +1,5 @@
 import type { Preview } from '@storybook/react';
+import previewTheme from './previewTheme.ts';
 import '../stories/assets/fonts.css';
 import '../stories/assets/all.min.css';
 import { withConsole } from '@storybook/addon-console';
@@ -11,6 +12,13 @@ const preview: Preview = {
         color: /(background|color)$/i,
         date: /Date$/i,
       },
+    },
+    backgrounds: {
+      default: 'Section',
+      values: [{ name: 'Section', value: 'rgba(0, 0, 0, 0.33)' }],
+    },
+    docs: {
+      theme: previewTheme,
     },
   },
   decorators: [(storyFn, context) => withConsole()(storyFn)(context)],
