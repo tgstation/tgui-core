69aa4c1897d9a09761eaa9c35f81164de18e1141
Kashargul <144968721+Kashargul@users.noreply.github.com>
2025-01-28 23:12:46 +0100

    style

--- lib/common/vector.ts
+++ lib/common/vector.ts
@@ -1,4 +1,4 @@
-import { zip } from "./collections";
+import { zip } from './collections';
 
 const ADD = (a: number, b: number): number => a + b;
 const SUB = (a: number, b: number): number => a - b;
@@ -9,33 +9,33 @@ export type Vector = number[];
 
 export function vecAdd(...vecs: Vector[]): Vector {
   return zip(...vecs).map((x) => x.reduce(ADD));
-};
+}
 
 export function vecSubtract(...vecs: Vector[]): Vector {
   return zip(...vecs).map((x) => x.reduce(SUB));
-};
+}
 
 export function vecMultiply(...vecs: Vector[]): Vector {
   return zip(...vecs).map((x) => x.reduce(MUL));
-};
+}
 
 export function vecDivide(...vecs: Vector[]): Vector {
   return zip(...vecs).map((x) => x.reduce(DIV));
-};
+}
 
 export function vecScale(vec: Vector, n: number): Vector {
   return vec.map((x) => x * n);
-};
+}
 
 export function vecInverse(vec: Vector): Vector {
   return vec.map((x) => -x);
-};
+}
 
 export function vecLength(vec: Vector): number {
   return Math.sqrt(vecMultiply(vec, vec).reduce(ADD));
-};
+}
 
 export function vecNormalize(vec: Vector): Vector {
   const length = vecLength(vec);
   return vec.map((c) => c / length);
-};
+}
