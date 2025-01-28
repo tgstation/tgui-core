9113ff068ae26219b04fab77516a906273cf8a68
Kashargul <144968721+Kashargul@users.noreply.github.com>
2025-01-28 21:04:31 +0100

    new line

--- lib/common/collections.ts
+++ lib/common/collections.ts
@@ -1,6 +1,7 @@
 type Zip<T extends unknown[][]> = {
   [K in keyof T]: T[K] extends (infer U)[] ? U : never;
 }[];
+
 /**
  * Creates an array of grouped elements, the first of which contains
  * the first elements of the given arrays, the second of which contains
