0f0e2ec316cb769ffe59b21bc89e8fecc78c03ff
robuddybot <65057909+robuddybot@users.noreply.github.com>
2025-03-12 21:59:48 +0000

    Build & Release v1.8.3

--- dist/common/ui.js
+++ dist/common/ui.js
@@ -1,61 +1,61 @@
 import { CSS_COLORS as u } from "./constants.js";
 import { classes as y } from "./react.js";
-const f = (t) => {
-  if (typeof t == "string")
-    return t.endsWith("px") ? `${Number.parseFloat(t) / 12}rem` : t;
-  if (typeof t == "number")
-    return `${t}rem`;
-}, p = (t) => {
-  if (typeof t == "string")
-    return f(t);
-  if (typeof t == "number")
-    return f(t * 0.5);
+const f = (o) => {
+  if (typeof o == "string")
+    return o.endsWith("px") ? `${Number.parseFloat(o) / 12}rem` : o;
+  if (typeof o == "number")
+    return `${o}rem`;
+}, p = (o) => {
+  if (typeof o == "string")
+    return f(o);
+  if (typeof o == "number")
+    return f(o * 0.5);
 };
-function w(t) {
-  return !h(t);
+function w(o) {
+  return !h(o);
 }
-function h(t) {
-  return typeof t == "string" && u.includes(t);
+function h(o) {
+  return typeof o == "string" && u.includes(o);
 }
-const l = (t) => (o, i) => {
-  (typeof i == "number" || typeof i == "string") && (o[t] = i);
-}, n = (t, o) => (i, e) => {
-  (typeof e == "number" || typeof e == "string") && (i[t] = o(e));
-}, s = (t, o) => (i, e) => {
-  e && (i[t] = o);
-}, c = (t, o, i) => (e, r) => {
+const c = (o) => (t, i) => {
+  (typeof i == "number" || typeof i == "string") && (t[o] = i);
+}, n = (o, t) => (i, e) => {
+  (typeof e == "number" || typeof e == "string") && (i[o] = t(e));
+}, l = (o, t) => (i, e) => {
+  e && (i[o] = t);
+}, s = (o, t, i) => (e, r) => {
   if (typeof r == "number" || typeof r == "string")
     for (let m = 0; m < i.length; m++)
-      e[t + i[m]] = o(r);
-}, a = (t) => (o, i) => {
-  w(i) && (o[t] = i);
+      e[o + i[m]] = t(r);
+}, a = (o) => (t, i) => {
+  w(i) && (t[o] = i);
 }, d = {
-  align: l("textAlign"),
+  align: c("textAlign"),
   bottom: n("bottom", f),
-  fontFamily: l("fontFamily"),
+  fontFamily: c("fontFamily"),
   fontSize: n("fontSize", f),
-  fontWeight: l("fontWeight"),
+  fontWeight: c("fontWeight"),
   height: n("height", f),
   left: n("left", f),
   maxHeight: n("maxHeight", f),
   maxWidth: n("maxWidth", f),
   minHeight: n("minHeight", f),
   minWidth: n("minWidth", f),
-  opacity: l("opacity"),
-  overflow: l("overflow"),
-  overflowX: l("overflowX"),
-  overflowY: l("overflowY"),
-  position: l("position"),
+  opacity: c("opacity"),
+  overflow: c("overflow"),
+  overflowX: c("overflowX"),
+  overflowY: c("overflowY"),
+  position: c("position"),
   right: n("right", f),
-  textAlign: l("textAlign"),
+  textAlign: c("textAlign"),
   top: n("top", f),
-  verticalAlign: l("verticalAlign"),
+  verticalAlign: c("verticalAlign"),
   width: n("width", f),
-  lineHeight: (t, o) => {
-    typeof o == "number" ? t.lineHeight = o : typeof o == "string" && (t.lineHeight = f(o));
+  lineHeight: (o, t) => {
+    typeof t == "number" ? o.lineHeight = t : typeof t == "string" && (o.lineHeight = f(t));
   },
   // Margin
-  m: c("margin", p, [
+  m: s("margin", p, [
     "Top",
     "Bottom",
     "Left",
@@ -65,10 +65,10 @@ const l = (t) => (o, i) => {
   ml: n("marginLeft", p),
   mr: n("marginRight", p),
   mt: n("marginTop", p),
-  mx: c("margin", p, ["left", "right"]),
-  my: c("margin", p, ["top", "bottom"]),
+  mx: s("margin", p, ["Left", "Right"]),
+  my: s("margin", p, ["Top", "Bottom"]),
   // Padding
-  p: c("padding", p, [
+  p: s("padding", p, [
     "Top",
     "Bottom",
     "Left",
@@ -78,58 +78,58 @@ const l = (t) => (o, i) => {
   pl: n("paddingLeft", p),
   pr: n("paddingRight", p),
   pt: n("paddingTop", p),
-  px: c("padding", p, ["left", "right"]),
-  py: c("padding", p, ["top", "bottom"]),
+  px: s("padding", p, ["Left", "Right"]),
+  py: s("padding", p, ["Top", "Bottom"]),
   // Color props
   color: a("color"),
   textColor: a("color"),
   backgroundColor: a("backgroundColor")
 }, b = {
-  bold: s("fontWeight", "bold"),
-  fillPositionedParent: (t, o) => {
-    o && (t.position = "absolute", t.top = 0, t.bottom = 0, t.left = 0, t.right = 0);
+  bold: l("fontWeight", "bold"),
+  fillPositionedParent: (o, t) => {
+    t && (o.position = "absolute", o.top = 0, o.bottom = 0, o.left = 0, o.right = 0);
   },
-  inline: s("display", "inline-block"),
-  italic: s("fontStyle", "italic"),
-  nowrap: s("whiteSpace", "nowrap"),
-  preserveWhitespace: s("whiteSpace", "pre-wrap")
+  inline: l("display", "inline-block"),
+  italic: l("fontStyle", "italic"),
+  nowrap: l("whiteSpace", "nowrap"),
+  preserveWhitespace: l("whiteSpace", "pre-wrap")
 };
-function S(t) {
-  const o = {}, i = {};
-  for (const e in t) {
+function T(o) {
+  const t = {}, i = {};
+  for (const e in o) {
     if (e === "style")
       continue;
-    const r = t[e], m = d[e] || b[e];
-    m ? m(i, r) : o[e] = r;
+    const r = o[e], m = d[e] || b[e];
+    m ? m(i, r) : t[e] = r;
   }
-  return o.style = { ...i, ...t.style }, o;
+  return t.style = { ...i, ...o.style }, t;
 }
-function T(t) {
-  const o = t.textColor || t.color, { backgroundColor: i } = t;
+function S(o) {
+  const t = o.textColor || o.color, { backgroundColor: i } = o;
   return y([
-    h(o) && `color-${o}`,
+    h(t) && `color-${t}`,
     h(i) && `color-bg-${i}`
   ]);
 }
-function P(t) {
-  const o = {};
-  if (!t) return o;
-  const i = t.split(" ");
+function P(o) {
+  const t = {};
+  if (!o) return t;
+  const i = o.split(" ");
   for (const e of i) {
     const [r, m] = e.split("-");
     if (r)
       if (r in d) {
         if (m === "") continue;
         const g = Number(m);
-        !Number.isNaN(g) && Number.isFinite(g) ? o[r] = g : o[r] = m;
-      } else r in b ? o[r] = !0 : console.warn(`Unknown prop ${r}`);
+        !Number.isNaN(g) && Number.isFinite(g) ? t[r] = g : t[r] = m;
+      } else r in b ? t[r] = !0 : console.warn(`Unknown prop ${r}`);
   }
-  return o;
+  return t;
 }
 export {
   b as booleanStyleMap,
-  T as computeBoxClassName,
-  S as computeBoxProps,
+  S as computeBoxClassName,
+  T as computeBoxProps,
   P as computeTwClass,
   p as halfUnit,
   d as stringStyleMap,
