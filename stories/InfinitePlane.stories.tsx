3fdb258a7818b7b9d944832348eef70ca13ed1b1
jlsnow301 <jlsnow301@pm.me>
2025-03-28 21:22:34 -0700

    initial commit

--- stories/InfinitePlane.stories.tsx
+++ stories/InfinitePlane.stories.tsx
@@ -1,6 +1,8 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import type { ComponentProps } from 'react';
+import { Icon } from '../lib/components/Icon';
 import { InfinitePlane } from '../lib/components/InfinitePlane';
+import gridBackground from './assets/grid_background.png';
 
 type StoryProps = ComponentProps<typeof InfinitePlane>;
 
@@ -11,8 +13,40 @@ export default {
 
 type Story = StoryObj<StoryProps>;
 
+const fontAwesomeEmoticons = [
+  'smile',
+  'frown',
+  'meh',
+  'laugh',
+  'sad-cry',
+  'angry',
+  'shocked',
+];
+
+function InfinitePlaneWrapper() {
+  return (
+    <div
+      style={{
+        height: 600,
+        width: 600,
+        border: '1px solid silver',
+      }}
+    >
+      <InfinitePlane
+        imageWidth={300}
+        backgroundImage={gridBackground}
+        onBackgroundMoved={(newX, newY) => console.log(newX, newY)}
+        initialLeft={165}
+        initialTop={250}
+      >
+        {fontAwesomeEmoticons.map((icon) => (
+          <Icon key={icon} name={icon} ml={5} style={{ color: 'orange' }} />
+        ))}
+      </InfinitePlane>
+    </div>
+  );
+}
+
 export const Default: Story = {
-  args: {
-    children: 'InfinitePlane',
-  },
+  render: () => <InfinitePlaneWrapper />,
 };
