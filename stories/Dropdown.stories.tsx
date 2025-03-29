883c891580b4d4b91f0a041154a67694ece33794
jlsnow301 <jlsnow301@pm.me>
2025-03-28 18:29:23 -0700

    stories

--- stories/Dropdown.stories.tsx
+++ stories/Dropdown.stories.tsx
@@ -1,6 +1,9 @@
 import type { Meta, StoryObj } from '@storybook/react';
-import type { ComponentProps } from 'react';
+import { type ComponentProps, type PropsWithChildren, useState } from 'react';
+
+import { Box } from '../lib/components/Box';
 import { Dropdown } from '../lib/components/Dropdown';
+import { Stack } from '../lib/components/Stack';
 
 type StoryProps = ComponentProps<typeof Dropdown>;
 
@@ -11,8 +14,84 @@ export default {
 
 type Story = StoryObj<StoryProps>;
 
-export const Default: Story = {
-  args: {
-    children: 'Dropdown',
+const defaultItems = [
+  {
+    value: 'item-1',
+    displayText: 'Item 1',
   },
+  {
+    value: 'item-2',
+    displayText: 'Item 2',
+  },
+  {
+    value: 'item-3',
+    displayText: 'Item 3',
+  },
+];
+
+type ShowcaseProps = {
+  title: string;
+} & PropsWithChildren;
+
+function Showcase(props: ShowcaseProps) {
+  const { title, children } = props;
+  return (
+    <Stack.Item mr={4}>
+      {title}
+      <Box mb={4} />
+      {children}
+    </Stack.Item>
+  );
+}
+
+function DropdownStories() {
+  const [selected, setSelected] = useState('');
+
+  const displayText = selected
+    ? defaultItems.find((item) => item.value === selected)?.displayText
+    : 'Dropdown';
+
+  return (
+    <Stack>
+      <Showcase title="Default">
+        <Dropdown
+          displayText={displayText}
+          options={defaultItems}
+          onSelected={setSelected}
+          selected={selected}
+        />
+      </Showcase>
+      <Showcase title="Over">
+        <Dropdown
+          over
+          displayText={displayText}
+          options={defaultItems}
+          onSelected={setSelected}
+          selected={selected}
+        />
+      </Showcase>
+      <Showcase title="Buttons">
+        <Dropdown
+          buttons
+          displayText={displayText}
+          options={defaultItems}
+          onSelected={setSelected}
+          selected={selected}
+        />
+      </Showcase>
+      <Showcase title="Disabled">
+        <Dropdown
+          disabled
+          displayText={displayText}
+          options={defaultItems}
+          onSelected={setSelected}
+          selected={selected}
+        />
+      </Showcase>
+    </Stack>
+  );
+}
+
+export const Default: Story = {
+  render: () => <DropdownStories />,
 };
