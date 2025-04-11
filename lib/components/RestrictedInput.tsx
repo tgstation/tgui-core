87fbdd2652012f89fcfefa41b4be8c81c03ee69e
Jeremiah <42397676+jlsnow301@users.noreply.github.com>
2025-04-10 19:57:16 -0700

    Rebuilds restrictedInput as a typescript FC (#137)

--- lib/components/RestrictedInput.tsx
+++ lib/components/RestrictedInput.tsx
@@ -1,53 +1,133 @@
-import { KEY } from '../common/keys';
-import type { BoxProps } from './Box';
-import { NumberInput } from './NumberInput';
+import { clamp } from 'lib/common/math';
+import { useEffect, useRef } from 'react';
+import { KEY, isEscape } from '../common/keys';
+import { classes } from '../common/react';
+import { Box, type BoxProps } from './Box';
 
 type Props = {
+  /** The current value of the input. */
   value: number;
 } & Partial<{
+  /** Restricted inputs round by default.  */
   allowFloats: boolean;
+  /** Focuses on mount. */
   autoFocus: boolean;
+  /** Selects any text in the input on mount. Assumes `autoFocus`. */
   autoSelect: boolean;
-  disabled: boolean;
+  /** Sets width to 100% of the parent. */
   fluid: boolean;
-  maxValue: number | null;
-  minValue: number | null;
-  onBlur: (e: Event, value: number) => void;
-  onChange: (e: Event, value: number) => void;
-  onEnter: (e: Event, value: number) => void;
+  /** Max value. 10,000 by default. */
+  maxValue: number;
+  /** Mark this if you want to use a monospace font. */
+  monospace: boolean;
+  /** Min value. 0 by default. */
+  minValue: number;
+  /** Called each time the value changes. */
+  onChange: (value: number) => void;
+  /** Called when the Enter key is pressed. Returns the current value. */
+  onEnter: (value: number) => void;
+  /** Called when the Escape key is pressed. */
+  onEscape: (value: number) => void;
 }> &
   BoxProps;
 
 /**
  * ## RestrictedInput
- * Creates an input which rejects improper keys.
- *
- * @deprecated Use `NumberInput` instead.
- *
- * This will server as a wrapper for NumberInput until removal. This decision was
- * made because it's poor UX. Users should be allowed to type in whatever they
- * want, but have the UI notify them it's invalid after it's entered.
- *
- * It also gives a false sense of security. It's just an annoying input.
+ * Creates a numerical input which rejects improper keys.
  */
 export function RestrictedInput(props: Props) {
-  const { minValue, maxValue, onChange, onEnter, onBlur, ...rest } = props;
+  const {
+    allowFloats,
+    autoFocus,
+    autoSelect,
+    className,
+    fluid,
+    maxValue = 10000,
+    minValue = 0,
+    monospace,
+    onChange,
+    onEnter,
+    onEscape,
+    ...rest
+  } = props;
+
+  const inputRef = useRef<HTMLInputElement>(null);
+  const innerValue = useRef(props.value);
+
+  function updateValue(value: number) {
+    if (value === innerValue.current) return;
+
+    let newValue = value;
+    if (Number.isNaN(value)) {
+      newValue = minValue;
+    } else if (!allowFloats) {
+      newValue = Math.round(value);
+    }
+
+    newValue = clamp(newValue, minValue, maxValue);
+    innerValue.current = newValue;
+    if (inputRef.current) {
+      inputRef.current.value = newValue.toString();
+    }
+    onChange?.(newValue);
+  }
+
+  function onChangeHandler(event: React.ChangeEvent<HTMLInputElement>) {
+    updateValue(Number(event.target.value));
+  }
+
+  function onKeyDownHandler(event: React.KeyboardEvent<HTMLInputElement>) {
+    if (event.key === KEY.Enter) {
+      event.preventDefault();
+      onEnter?.(innerValue.current);
+      inputRef.current?.blur();
+    } else if (isEscape(event.key)) {
+      event.preventDefault();
+      onEscape?.(innerValue.current);
+      inputRef.current?.blur();
+    }
+  }
+
+  useEffect(() => {
+    let timeout: NodeJS.Timeout;
+
+    if (autoFocus || autoSelect) {
+      timeout = setTimeout(() => {
+        inputRef.current?.focus();
+        if (autoSelect) {
+          inputRef.current?.select();
+        }
+      }, 1);
+    }
+
+    return () => clearTimeout(timeout);
+  }, []);
+
+  useEffect(() => {
+    updateValue(props.value);
+  }, [props.value]);
 
   return (
-    <NumberInput
+    <Box
+      className={classes([
+        'Input',
+        fluid && 'Input--fluid',
+        monospace && 'Input--monospace',
+        className,
+      ])}
       {...rest}
-      minValue={minValue ?? 0}
-      maxValue={maxValue ?? Number.POSITIVE_INFINITY}
-      onChange={(value) => {
-        onChange?.({} as Event, value);
-      }}
-      onKeyDown={(e) => {
-        if (e.key === KEY.Enter) {
-          // @ts-expect-error
-          onEnter?.({} as Event, Number(e.currentTarget.value));
-        }
-      }}
-      step={1}
-    />
+    >
+      <div className="Input__baseline">.</div>
+      <input
+        className={classes(['Input__input', 'RestrictedInput'])}
+        max={maxValue}
+        min={minValue}
+        onChange={onChangeHandler}
+        onKeyDown={onKeyDownHandler}
+        ref={inputRef}
+        type="number"
+        value={innerValue.current}
+      />
+    </Box>
   );
 }
--- stories/RestrictedInput.stories.tsx
+++ stories/RestrictedInput.stories.tsx
@@ -1,4 +1,5 @@
 import type { Meta, StoryObj } from '@storybook/react';
+import { Button } from 'lib/components/Button';
 import type { ComponentProps } from 'react';
 import { useState } from 'react';
 import { RestrictedInput } from '../lib/components/RestrictedInput';
@@ -16,13 +17,73 @@ export const Default: Story = {
   render: () => {
     const [value, setValue] = useState(1);
 
-    console.log(value);
-
     return (
       <RestrictedInput
         value={value}
-        onChange={(e, value) => setValue(value)}
+        onChange={(value) => setValue(value)}
         width={8}
+      />
+    );
+  },
+};
+
+export const Fluid: Story = {
+  render: () => {
+    const [value, setValue] = useState(1);
+
+    return (
+      <RestrictedInput
+        fluid
+        value={value}
+        onChange={(value) => setValue(value)}
+      />
+    );
+  },
+};
+
+export const WithFloats: Story = {
+  render: () => {
+    const [value, setValue] = useState(0.5);
+
+    return (
+      <RestrictedInput
+        allowFloats
+        value={value}
+        onChange={(value) => setValue(value)}
+        width={8}
+        maxValue={3}
+      />
+    );
+  },
+};
+
+export const SendParentValue: Story = {
+  render: () => {
+    const [parentValue, setParentValue] = useState(5);
+
+    return (
+      <>
+        <Button onClick={() => setParentValue(0)}>Min</Button>
+        <RestrictedInput
+          value={parentValue}
+          onChange={(value) => setParentValue(value)}
+          width={8}
+        />
+        <Button onClick={() => setParentValue(103000)}>Max</Button>
+      </>
+    );
+  },
+};
+
+export const AutoSelect: Story = {
+  render: () => {
+    const [value, setValue] = useState(1);
+
+    return (
+      <RestrictedInput
+        autoSelect
+        value={value}
+        onChange={(value) => setValue(value)}
       />
     );
   },
