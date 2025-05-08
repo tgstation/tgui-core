5c425712dde6475b4f2c9b9216e058f70d780ec4
Aylong <alexanderkitsa@gmail.com>
2025-05-08 09:47:05 +0300

    Port stickykey fix from CM

--- lib/common/hotkeys.ts
+++ lib/common/hotkeys.ts
@@ -177,13 +177,23 @@ export function setupHotKeys() {
   globalEvents.on('window-blur', () => {
     releaseHeldKeys();
   });
-  globalEvents.on('key', (key: KeyEvent) => {
-    for (const keyListener of keyListeners) {
-      keyListener(key);
-    }
-    handlePassthrough(key);
-  });
-}
+  startKeyPassthrough();
+};
+
+export function startKeyPassthrough ()  {
+  globalEvents.on('key', keyEvent);
+};
+
+export function stopKeyPassthrough()  {
+  globalEvents.off('key', keyEvent);
+};
+
+function keyEvent(key: KeyEvent) {
+  for (const keyListener of keyListeners) {
+    keyListener(key);
+  }
+  handlePassthrough(key);
+};
 
 /**
  * Registers for any key events, such as key down or key up.
