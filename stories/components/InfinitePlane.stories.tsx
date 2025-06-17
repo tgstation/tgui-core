754dc6e874134f5539c4be1e51027b4067682422
SmArtKar <44720187+SmArtKar@users.noreply.github.com>
2025-06-17 04:25:56 +0200

    Makes InfinitePlane smoother, fixes zoom centering around (0, 0) and implements external position controls (#200)

--- stories/components/InfinitePlane.stories.tsx
+++ stories/components/InfinitePlane.stories.tsx
@@ -1,5 +1,5 @@
-import { Icon, InfinitePlane } from '@components';
-import type { ComponentProps } from 'react';
+import { Button, Icon, InfinitePlane } from '@components';
+import { type ComponentProps, useState } from 'react';
 import type { Meta, StoryObj } from 'storybook-react-rsbuild';
 import gridBackground from '../../static/grid_background.png';
 
@@ -23,29 +23,45 @@ const fontAwesomeEmoticons = [
 ];
 
 export const Default: Story = {
-  render: () => (
-    <div
-      style={{
-        border: '1px solid silver',
-        height: 600,
-        width: 600,
-      }}
-    >
-      <InfinitePlane
-        backgroundImage={gridBackground}
-        imageWidth={300}
-        initialLeft={165}
-        initialTop={250}
-        onBackgroundMoved={(newX, newY) => console.log(newX, newY)}
-      >
-        {fontAwesomeEmoticons.map((icon, index) => (
-          <Icon
-            key={icon}
-            name={icon}
-            style={{ color: 'orange', left: index * 50, position: 'absolute' }}
-          />
-        ))}
-      </InfinitePlane>
-    </div>
-  ),
+  render: () => {
+    const [coords, setCoords] = useState([165, 250]);
+
+    return (
+      <div>
+        <Button onClick={() => setCoords([0, 0])}>Jump to (0, 0)</Button>
+        <Button onClick={() => setCoords([300, 300])}>
+          Jump to (300, 300)
+        </Button>
+        <div
+          style={{
+            border: '1px solid silver',
+            height: 600,
+            width: 600,
+          }}
+        >
+          <InfinitePlane
+            backgroundImage={gridBackground}
+            imageWidth={300}
+            initialLeft={165}
+            initialTop={250}
+            zoomToX={coords[0]}
+            zoomToY={coords[1]}
+            onBackgroundMoved={(newX, newY) => console.log(newX, newY)}
+          >
+            {fontAwesomeEmoticons.map((icon, index) => (
+              <Icon
+                key={icon}
+                name={icon}
+                style={{
+                  color: 'orange',
+                  left: index * 50,
+                  position: 'absolute',
+                }}
+              />
+            ))}
+          </InfinitePlane>
+        </div>
+      </div>
+    );
+  },
 };
