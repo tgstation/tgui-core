5f7c5d8f66a11e7555115a8dbe63a84806813b6f
John Willard <53777086+JohnFulpWillard@users.noreply.github.com>
2025-12-02 03:55:34 -0500

    Update color.ts

--- lib/common/color.ts
+++ lib/common/color.ts
@@ -13,22 +13,30 @@ export class Color {
     this.a = a;
   }
 
-  toString() {
-    return `rgba(${this.r | 0}, ${this.g | 0}, ${this.b | 0}, ${this.a | 0})`;
+  toString(): string {
+    // Alpha component needs to permit fractional values, so cannot use |
+    let alpha = this.a;
+    if (typeof alpha === 'string') {
+      alpha = Number.parseFloat(this.a as any);
+    }
+    if (Number.isNaN(alpha)) {
+      alpha = 1;
+    }
+    return `rgba(${this.r | 0}, ${this.g | 0}, ${this.b | 0}, ${alpha})`;
   }
 
-  // Darkens a color by a given percent. Returns a color, which can have toString called to get it's rgba() css value.
+  /**  Darkens a color by a given percent. Returns a color, which can have toString called to get it's rgba() css value. */
   darken(percent: number): Color {
-    percent /= 100;
+	const scaled = percent / 100;
     return new Color(
-      this.r - this.r * percent,
-      this.g - this.g * percent,
-      this.b - this.b * percent,
+      this.r - this.r * scaled,
+      this.g - this.g * scaled,
+      this.b - this.b * scaled,
       this.a,
     );
   }
 
-  // Brightens a color by a given percent. Returns a color, which can have toString called to get it's rgba() css value.
+  /** Brightens a color by a given percent. Returns a color, which can have toString called to get it's rgba() css value. */
   lighten(percent: number): Color {
     // No point in rewriting code we already have.
     return this.darken(-percent);
@@ -38,11 +46,22 @@ export class Color {
    * Creates a color from the CSS hex color notation.
    */
   static fromHex(hex: string): Color {
-    return new Color(
-      parseInt(hex.substr(1, 2), 16),
-      parseInt(hex.substr(3, 2), 16),
-      parseInt(hex.substr(5, 2), 16),
-    );
+    if (hex.length === 7) {
+      return new Color(
+        Number.parseInt(hex.slice(1, 3), 16),
+        Number.parseInt(hex.slice(3, 5), 16),
+        Number.parseInt(hex.slice(5, 7), 16),
+      );
+    }
+    if (hex.length === 9) {
+      return new Color(
+        Number.parseInt(hex.slice(1, 3), 16),
+        Number.parseInt(hex.slice(3, 5), 16),
+        Number.parseInt(hex.slice(5, 7), 16),
+        Number.parseInt(hex.slice(7, 9), 16) / 256,
+      );
+    }
+    throw new Error('Invalid hex color format. Expected #RRGGBB or #RRGGBBAA.');
   }
 
   /**
@@ -93,11 +112,7 @@ export class Color {
  * SOFTWARE.
  */
 
-const round = (
-  number: number,
-  digits = 0,
-  base = 10 ** digits,
-): number => {
+const round = (number: number, digits = 0, base = 10 ** digits): number => {
   return Math.round(base * number) / base;
 };
 
@@ -313,9 +328,7 @@ const format = (number: number) => {
 
 export const rgbaToHex = ({ r, g, b, a }: RgbaColor): string => {
   const alphaHex = a < 1 ? format(round(a * 255)) : '';
-  return (
-    `#${format(round(r))}${format(round(g))}${format(round(b))}${alphaHex}`
-  );
+  return `#${format(round(r))}${format(round(g))}${format(round(b))}${alphaHex}`;
 };
 
 export const rgbaToHsva = ({ r, g, b, a }: RgbaColor): HsvaColor => {
